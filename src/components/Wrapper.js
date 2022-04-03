235a75ed1d36cb7c947db3f774d93c0a35c79185
Galatée Lardière <galatee.lardiere@gmail.com>
2022-04-03 14:56:54 +0200

    Mep d'une condition pour le bon renvoie des données y compris à la décoche de la case (demande de retour à l'état initial des données) - en 2 étapes

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -12,11 +12,14 @@ function Wrapper() {
     //STEP 5 | Mettre un écouteur d'évènement sur lequel on va appeler une méthode (ici, "handleAvailability") pour manipuler nos données par la suite. Cette méthode pour l'instant est juste nommée mais inconnue
     //STEP 6 | On déclare uen fonction nommée "handleAvailability", on lui passe en argument notre évènement. On fait un console.log pour vérifier le bon cablâge (on à connecté le fait de cocher la checkboxet de produire du code en réponse à ce clic)
     
-    //STEP 7 | Faire un console.log(e) de l'évènement (qui est un sipmple objet) afin d'accéder à un certain nb d'infos sur l'évènement. Ce qui nous interesse est la clé "target", qui correspond à l'élément sur lequel on clique
+    //STEP 7 | Faire un console.log(e) de l'évènement (qui est un simple objet) afin d'accéder à un certain nb d'infos sur l'évènement. Ce qui nous interesse est la clé "target", qui correspond à l'élément sur lequel on clique
     //STEP 8 | Faire un console.log(target) afin de lire des informations sur ce target/ puis sur la value de target par exemple ou encore checked (ce qui nous interesse ici), on remarque alors que checked est soit true soit false en fonction de son état
     //STEP 9 | Appliquer une methode filter sur notre tableau de données et l'on souhiate afficher que les maisons available. house.available doit être égal  l'état de notre case à cocher (cad e.target.checked)
     //STEP 10 | Contrôler avec un console.log de notre méthode. On vient donc de récupérer un nouveau tableau contenant uniquement lensemble des maisons dispo
-    // STEP 11 | Mettre à jour la clé "houses" grâce au setter afin que notre filtre s'affiche
+    //STEP 11 | Mettre à jour la clé "houses" grâce au setter afin que notre filtre s'affiche
+    
+    //STEP 13 | Pour + de faciliter car on va l'utiliser plusieurs fois, on met "e.target.checked" dans une variable.
+    //STEP 14 | Mettre une condition car à la décoche de la case, cela nous renvoie que les false et non l'état intial de nos données. On vérifie donc avec "if" : si notre case est cochée -> on veut filtrer et si non -> on va réutiliser notre setter pour réinitialiser l'état à l'intégralité de notre tableau de données.
     
     const [houses, setHouses] = useState(housesToRent)
 
@@ -27,8 +30,18 @@ function Wrapper() {
         //console.log(e.target.value);
         //console.log(e.target.checked)
         //console.log(housesToRent.filter(house => house.available === e.target.checked));
-        setHouses(housesToRent.filter(house => house.available === e.target.checked));
+        //setHouses(housesToRent.filter(house => house.available === e.target.checked));
+
+        let checked = e.target.checked;
+
+        if(checked) {
+            setHouses(housesToRent.filter(house => house.available === checked))
+        }else{
+            setHouses(housesToRent)
+        }
+        
     }
+
 
 	return (
 		<div className="wrapper">
