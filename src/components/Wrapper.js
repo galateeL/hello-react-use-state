dec76a4d2003eafc6ea008e14f2c048ff248de56
Galatée Lardière <galatee.lardiere@gmail.com>
2022-04-03 12:37:44 +0200

    Déclaration et initialisation d'un state en 4 étapes

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import Cards from "./Cards";
-import housesToRent from "../data/housesToRent";
+import housesToRent from "../data/housesToRent"; //récupération des données depuis data
 
 function Wrapper() {
+    
+    //STEP 1 | Déclaration d'un state - cela prend la forme d'une constante avec houses (nom de la donnée que l'on veut manipuler) et setHouses (notre setter = une petite fonction qui met à jour notre donnée)
+    //STEP 2 | En paramètre du useState on met la valeur d'initialisation de notre variable "houses". Ici on initialise useState avec nos données complètes (notre tableau de données)
+    //STEP 3 | Il faut importer {useState} (ligne 1)
+    //STEP 4 | On passe au composant Cards (bas de page) houses car nous venons d'initialiser avec housesTorent
+    const [houses, setHouses] = useState(housesToRent)
+
 	return (
 		<div className="wrapper">
 			<div className="filters">
@@ -12,24 +19,25 @@ function Wrapper() {
 					placeholder="🔎 Type to search"
 				/>
 
-        <select className="select" name="home">
-            <option value="All">All</option>
-            <option value="Flat">Flat</option>
-            <option value="House">House</option>
-        </select>
+                <select className="select" name="home">
+                    <option value="All">All</option>
+                    <option value="Flat">Flat</option>
+                    <option value="House">House</option>
+                </select>
 
-        <div>
-            <label htmlFor="available">Show only Available</label>
-            <input
-            type="checkbox"
-            className="available-checkbox"
-            name="checkbox"
-            id=""
-            />
-        </div>
-  		</div>
+                <div>
+                    <label htmlFor="available">Show only Available</label>
+                    <input
+                    type="checkbox"
+                    className="available-checkbox"
+                    name="checkbox"
+                    id=""
+                    />
+                </div>
 
-			<Cards houses={housesToRent} />
+  		    </div>
+
+			<Cards houses={houses} />
 		</div>
 	);
 }
